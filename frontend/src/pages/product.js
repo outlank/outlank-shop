2ac6ac22edbb1e81596d56486946ebd090a55088
fishyu <ifeinstein@163.com>
2018-08-02 10:15:28 +0800

    save

--- frontend/src/pages/product.js
+++ frontend/src/pages/product.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Drawer, List, NavBar, Icon } from 'antd-mobile'
+import { Modal, List, Button, WhiteSpace, WingBlank } from 'antd-mobile'
 import './product.css'
 
 class page extends React.Component {
@@ -33,19 +33,23 @@ class page extends React.Component {
 
     return (
       <div className='product-page'>
-        <NavBar icon={<Icon type='ellipsis' />} onLeftClick={onOpenChange}>Basic</NavBar>
-        <Drawer
-          className='product-drawer'
-          style={{ minHeight: document.documentElement.clientHeight }}
-          enableDragHandle
-          position='bottom'
-          contentStyle={{ color: '#A6A6A6', textAlign: 'center', paddingTop: 42 }}
-          sidebar={sidebar}
-          open={this.state.open}
-          onOpenChange={this.onOpenChange}
+        <Button onClick={this.showModal('modal2')}>popup</Button>
+        <WhiteSpace />
+        <Modal
+          popup
+          visible={this.state.modal2}
+          onClose={this.onClose('modal2')}
+          animationType="slide-up"
         >
-          Click upper-left corner
-        </Drawer>
+          <List renderHeader={() => <div>委托买入</div>} className="popup-list">
+            {['股票名称', '股票代码', '买入价格'].map((i, index) => (
+              <List.Item key={index}>{i}</List.Item>
+            ))}
+            <List.Item>
+              <Button type="primary" onClick={this.onClose('modal2')}>买入</Button>
+            </List.Item>
+          </List>
+        </Modal>
       </div>
     )
   }
